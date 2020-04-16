a47b6dc8b080a1b9073292d6e3472c63617174c3
Tyler Buchman <tyler_buchman@jabil.com>
2020-04-16 19:15:01 -0400

    Remove wait_for_occupancy_map from octomap
    This is no longer needed in the octommap and is managed by node_3d.
    
    Improve whitespace in particle filter and point cloud scanner.

--- src/amcl/sensors/point_cloud_scanner.cpp
+++ src/amcl/sensors/point_cloud_scanner.cpp
@@ -238,14 +238,14 @@ double PointCloudScanner::recalcWeight(std::shared_ptr<PFSampleSet> set)
   return rv;
 }
 
-void PointCloudScanner::getMapCloud(std::shared_ptr<PointCloudData> data,
-                                    PFVector pose, pcl::PointCloud<pcl::PointXYZ>& map_cloud)
+void PointCloudScanner::getMapCloud(std::shared_ptr<PointCloudData> data, PFVector pose,
+                                    pcl::PointCloud<pcl::PointXYZ>& map_cloud)
 {
   tf::Vector3 footprint_to_map_origin(pose.v[0], pose.v[1], 0.0);
   tf::Quaternion footprint_to_map_q(tf::Vector3(0.0, 0.0, 1.0), pose.v[2]);
   tf::Transform footprint_to_map_tf(footprint_to_map_q, footprint_to_map_origin);
-  pcl_ros::transformPointCloud(data->points_, map_cloud,
-                               point_cloud_scanner_to_footprint_tf_ * footprint_to_map_tf);
+  tf::Transform point_cloud_scanner_to_map_tf = point_cloud_scanner_to_footprint_tf_ * footprint_to_map_tf;
+  pcl_ros::transformPointCloud(data->points_, map_cloud, point_cloud_scanner_to_map_tf);
 }
 
 double PointCloudScanner::applyGompertz(double p)
