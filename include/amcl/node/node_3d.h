248ae57a22f7252f2d50e0bba4b7b6fa66c46419
Tyler Buchman <tyler_buchman@jabil.com>
2025-02-03 23:48:15 -0500

    Publish best fit cloud and pose array
    Publish the point cloud from the perspective of the particle with the
    best fit for the latest scan.
    
    Publish a pose array of the particles with the best fit for the latest
    scan.

--- include/amcl/node/node_3d.h
+++ include/amcl/node/node_3d.h
@@ -136,6 +136,7 @@ private:
   double global_localization_off_map_factor_;
   double global_localization_non_free_space_factor_;
   bool global_localization_active_;
+  int num_best_fit_particles_;
 };
 
 }  // namespace amcl
